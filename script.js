d67d1d11cc6959b8929c9eb9b8faba316ce68db7
tilak-c <66167345+tilak-c@users.noreply.github.com>
2025-10-17 11:47:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function volume_sphere() {
     //Write your code here
+	event.preventDefault();
+	const radiusValue=parseFloat(document.getElementById("radius").value);
+	const result=4/3*3.14*radiusValue*radiusValue*radiusValue;
+	const volumeValue=document.getElementById("volume")
+	const buttonSubmit=document.getElementById("submit");
+	// buttonSubmit.addEventListener("click",function(){
+	if(result<0){
+		volumeValue.value=NaN;
+		return;
+	}
+	volumeValue.value=result.toFixed(4);	
+	// })
   
 } 
 
